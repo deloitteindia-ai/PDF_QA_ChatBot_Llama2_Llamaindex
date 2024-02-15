dab885bd423801cfa35740978c81c60f0bf92357
Arijit Dey <111451768+arijitaurko@users.noreply.github.com>
2024-02-15 21:04:20 +0530

    Update llama2-pdf-streamlit.py

--- llama2-pdf-streamlit.py
+++ llama2-pdf-streamlit.py
@@ -3,8 +3,8 @@ import os
 import streamlit as st
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
-#from llama_index.core import set_global_service_context
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, ServiceContext
+from llama_index import set_global_service_context
 from llama_index.embeddings import GradientEmbedding
 from llama_index.llms import GradientBaseModelLLM
 from llama_index.vector_stores import CassandraVectorStore
@@ -64,7 +64,7 @@ def main():
     chunk_size=256,
     pydantic_program_mode = "llm")
 
-    #set_global_service_context(service_context)
+    set_global_service_context(service_context)
 
     with st.sidebar:
         st.subheader('Upload Your PDF File')

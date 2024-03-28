88a61dc4fb557091631d5dd6b4d7e389f22174fa
Arijit Dey <111451768+arijitaurko@users.noreply.github.com>
2024-03-28 15:06:02 +0530

    Update llama2-pdf-streamlit.py

--- llama2-pdf-streamlit.py
+++ llama2-pdf-streamlit.py
@@ -115,7 +115,7 @@ def main():
     if st.session_state.activate_chat == True:
         selected_question = st.selectbox('Select a default question', [""] + default_questions)
         if selected_question:
-            st.session_state.prompt = selected_question
+            st.session_state.prompt = st.chat_input(selected_question)
         else:
             st.session_prompt = st.chat_input("Ask your question from the PDF?")           
         if st.session_state.prompt:
